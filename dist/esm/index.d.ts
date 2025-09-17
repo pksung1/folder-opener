20ca2d4ce0c2c6a36c628a933458090a84df786a
pksung1 <pksung1@gmail.com>
2025-09-17 14:34:42 +0900

    Remove dist folder from git tracking

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { FolderOpenerPlugin } from './definitions.js';
-declare const FolderOpener: FolderOpenerPlugin;
-export * from './definitions.js';
-export { FolderOpener };
--- dist/esm/index.js
+++
@@ -1,5 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const FolderOpener = registerPlugin('FolderOpener');
-export * from './definitions.js';
-export { FolderOpener };
-//# sourceMappingURL=index.js.map
